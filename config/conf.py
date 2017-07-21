5f7dd4de315f9a7968efb19044565b8ef09f7925
Delphi <renxiaopeng@newdun.com>
2017-07-21 14:26:50 +0800

    ad

---
+++ config/conf.py
@@ -0,0 +1,26 @@
+# encoding=utf-8
+"""settings file"""
+
+REDIS_CONF = {
+     "host": "localhost",
+     "port": 6379,
+     "db": 0,
+     "password": ""
+     # "password": "shdj$%26W3!@e"
+
+}
+
+TOKENS = {
+    "192.168.199.124": "NuFOOb2OokoO2YrI6DkNHqWjBXUhvZdV",
+    "192.168.199.123": "NuFOOb2OokoO2YrI6DkNHqWjBXUhvZdV",
+    "127.0.0.1": "NuFOOb2OokoO2YrI6DkNHqWjBXUhvZdV"
+}
+
+ERROR = {
+    "Succeed": 1000,
+    "Quering": 10000,
+    "Unknow": 10001,
+    "LackParam": 10002
+}
+
+config = {}
